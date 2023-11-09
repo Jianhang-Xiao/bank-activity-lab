02f9c3be1f20433530b2a33c1da0ed9a4fb691c7
Jianhang Xiao <xiaojianhang@yahoo.com>
2023-11-08 22:49:33 -0500

    stock

--- code/StockMetrics.py
+++ code/StockMetrics.py
@@ -19,9 +19,9 @@ class StockMetrics(StockData): #interitance in stockData code allows the "StockM
         averages = []
         for row in self.data:
             #print("old row", row)
-            new_row=[float(val) for val in row[1:] if val !="" and val !=" "]#use list comprehenstion to create a new list (“new_row”) containing the floating point that is non-empty and non-space values in the sub list “row” staring from the second element.
-            x=stats.mean(new_row)
-            averages.append(round(x,3))
+            new_row=[float(val) for val in row[1:] if val !="" and val !=" "] #use list comprehenstion to create a new list (“new_row”) containing the floating point that is non-empty and non-space values in the sub list “row” staring from the second element.
+            x=stats.mean(new_row) #calculate the mean of the values in the new_row list
+            averages.append(round(x,3)) #round the result to three decimal places, and then append the rounded mean to the averages list
         return averages
 
 
